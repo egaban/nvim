e5a5a8f2f9329ca72b517a4e4a3f3cc7dee217fc
Enzo Gaban <enzogaban@gmail.com>
2024-03-11 21:30:16 -0300

    disable hlsearch

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,10 +1,17 @@
 local M = {}
 
 M.ui = {
-  theme = "rosepine",
-  tabufline = {
-    enabled = false
-  }
+	theme = "rosepine",
+	tabufline = {
+		enabled = false,
+	},
 }
+
+-- Snippets
+local nvim_config = vim.fn.stdpath("config")
+vim.g.vscode_snippets_path = nvim_config .. "/snippets"
+
+-- Disable search highlight
+vim.opt.hlsearch = false
 
 return M
