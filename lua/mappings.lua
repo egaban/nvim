9a952a8c44343207f2a229df3e5067acf76bfe51
Enzo Gaban <enzogaban@gmail.com>
2024-03-24 16:59:25 -0300

    formatting

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,11 +1,11 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
 local map = vim.keymap.set
 
 map("n", "<leader>fm", function()
-  require("conform").format()
+	require("conform").format()
 end, { desc = "File Format with conform" })
 
 map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
