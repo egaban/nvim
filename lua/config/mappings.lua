bf7491f1cf39923a3fb959bf1fec26002e8e9e20
Enzo Gaban <git.gaban@pm.me>
2026-02-06 20:57:04 -0300

    feat: rename mapping

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -15,6 +15,7 @@ map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 -- LSP
 map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 map("n", "gD", vim.lsp.buf.definition, { desc = "Go to declaration" })
+map("n", "cd", vim.lsp.buf.rename, { desc = "Change definition" })
 map("n", "gy", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
 map("n", "gI", vim.lsp.buf.type_definition, { desc = "Go to implementation" })
 map("n", "gA", vim.lsp.buf.type_definition, { desc = "All references" })
