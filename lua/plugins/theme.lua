885c3a731a64c4a6d545e6a3c14827ddc326ecd1
Enzo Gaban <git.gaban@pm.me>
2026-02-10 20:47:54 -0300

    nicer rose-pine

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,33 @@
 return {
 	"rose-pine/neovim",
 	name = "rose-pine",
-	dependencies = { "nyoom-engineering/oxocarbon.nvim", "shaunsingh/nord.nvim" },
 	lazy = false,
+	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme rose-pine")
+		require("rose-pine").setup({
+			styles = {
+				italic = false,
+			},
+			palette = {
+				main = {
+					base = "#0D1017",
+				},
+			},
+			highlight_groups = {
+				TelescopeNormal = { bg = "base" },
+				TelescopeBorder = { fg = "overlay", bg = "base" },
+				TelescopePromptNormal = { bg = "base" },
+				TelescopePromptBorder = { fg = "overlay", bg = "base" },
+				TelescopePromptTitle = { fg = "love", bg = "base" },
+				TelescopePreviewNormal = { bg = "base" },
+				TelescopePreviewBorder = { fg = "overlay", bg = "base" },
+				TelescopePreviewTitle = { fg = "iris", bg = "base" },
+				TelescopeResultsNormal = { bg = "base" },
+				TelescopeResultsBorder = { fg = "overlay", bg = "base" },
+				TelescopeResultsTitle = { fg = "foam", bg = "base" },
+			},
+		})
+
+		vim.cmd.colorscheme("rose-pine")
 	end,
 }
